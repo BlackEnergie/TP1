59c670f76bb6c7c14f2925e287325c871fc7cde8
BlackEnergie <the.perrin@outlook.fr>
2017-09-18 17:02:55 +0200

    maj 18/09

--- src/DemoArrayList.java
+++ src/DemoArrayList.java
@@ -2,19 +2,28 @@ import java.util.ArrayList;
 
 public class DemoArrayList 
 {
-	public static void main(String[] args) 
-	{
-		ArrayList<String> amis = new ArrayList<String>();
-		amis.add("Marie");
-		amis.add("Anne");
-		amis.add("David");
-		amis.add("Rémy");
+	public static void main(String[] args){
+		ArrayList<Pays> LesPays = new ArrayList<Pays>();
 		
-		int nombreAmis = amis.size();
+		Pays France = new Pays("France");
+		LesPays.add(France);
 		
-		for(int i=0; i<nombreAmis; i++) 
-		{
-			System.out.println(amis.get(i));
+		Pays Allemagne = new Pays("Allemagne");
+		LesPays.add(Allemagne);
+		
+		Pays USA = new Pays("Etats-Unis");
+		LesPays.add(USA);
+		
+		Pays Chine = new Pays("Chine");
+		LesPays.add(Chine);
+		
+		int nbPays = LesPays.size();
+		
+		System.out.println("La collection contient " + LesPays.size() + " pays");
+		for(int i=0; i<nbPays; i++) {
+			System.out.println(LesPays.get(i).getNom());
 		}
+		
+		LesPays.get(1).setNom("Russie");
 	}
 }
---
+++ src/Pays.java
@@ -0,0 +1,19 @@
+
+public class Pays {
+	private String nom;
+	
+	public Pays(String nomPays) {
+		this.setNom(nomPays);
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	
+}
